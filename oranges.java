343f0147843fd827b69599e27f32f6dc4441862e
Naveen Ganessin <naveenganessin@gmail.com>
2014-07-27 22:18:48 +0800

    Applet main
    Main and class files

---
+++ oranges.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.awt.*;
+import javax.swing.*;
+
+public class oranges extends JApplet {
+
+	public static void main(String[] args) {
+		
+		narvancy w = new narvancy();
+		w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		w.setSize(230,200);
+		w.setVisible(true);
+		
+	}
+	
+	
+}
